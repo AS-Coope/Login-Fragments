166382568f9e29e1003389da8b1f64aa785b35f6
AS-Coope <deltazirconium@gmail.com>
2023-09-15 14:24:44 -0500

    Create button on login page and add simple click functionality

--- app/src/main/java/com/example/loginfragments/LoginFragment.kt
+++ app/src/main/java/com/example/loginfragments/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.loginfragments.databinding.FragmentLoginBinding
 import com.google.android.material.textfield.TextInputEditText
@@ -14,6 +16,7 @@ class LoginFragment : Fragment() {
 
     private lateinit var username: TextInputEditText
     private lateinit var password: TextInputEditText
+    private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +34,17 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         username = binding.usernameEditText
         password = binding.passwordEditText
+        loginButton = binding.loginBtn
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        loginButton?.setOnClickListener{
+            Toast.makeText(requireContext(), "Login Button works", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun onDestroyView() {
